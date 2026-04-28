d7dd4df3c1a963030a5e4f179a9b8f5c65c0ad57
すえきち <mojiryouta@gmail.com>
2026-04-28 09:36:04 +0900

    regression ver4

--- run_regression.py
+++ run_regression.py
@@ -9,6 +9,8 @@ X, Y = datasets.load_linear_example()
 model = regression.LinearRegression()
 model.fit(X, Y)
 
-print(model.theta)
-print(model.predict(X))
+# testing
+print(model.theta)          # ver2
+print(model.predict(X))     # ver3
+print(model.score(X, Y))    # ver4
 
